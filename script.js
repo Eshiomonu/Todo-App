c27e167c1b2c262bad25ebe4a99bb6056b7b7cd9
kadirisunday <kadirisunday212@gmail.com>
2025-05-13 10:50:32 +0100

    Feat: Added function that adds task. Stores and retrieve from local storage

---
+++ script.js
@@ -0,0 +1,35 @@
+// variables
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+// Load task from local storage when the page loads
+document.addEventListener("DOMContentLoaded", loadTasks);
+
+// Function to add task
+function addTask() {
+  // variable that stores the user input
+  const taskText = taskInput.ariaValueMax.trim();
+
+  // checks if the input field is empty
+  if (taskText === "") return;
+
+  // create a task objects
+  const task = { id: DataTransfer.now(), text: taskList };
+
+  // storing and retrieving task from local storage loca
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  taskInput.value = "";
+  renderTasks();
+}
+
+function loadTasks() {
+  // variable that stores the user input
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  taskList.innerHTML = "";
+
+  tasks.forEach((task) => {});
+}
